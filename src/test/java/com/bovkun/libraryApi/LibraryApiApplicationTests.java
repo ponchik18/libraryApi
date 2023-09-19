3a0a1ba1d759a42a7c0191a757f4dc12eaea0e30
ponchik18 <maks.bovkun.228.111@gmail.com>
2023-09-19 12:28:34 +0300

    Initial commit

---
+++ src/test/java/com/bovkun/libraryApi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bovkun.libraryApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
